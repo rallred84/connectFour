39fac5b33caee4c35e8ce4d0d87c26926abf78a5
Robert Allred <rallred84@gmail.com>
2023-03-12 11:03:36 -0500

    Changed up the create board function
    The original one was a collection of rows that made up the board, but since this game is played by dropping tokens into the columns, it made more sense for the board to be a collection of columns instead of rows

--- main.js
+++ main.js
@@ -4,26 +4,33 @@ let columns = 7;
 const playArea = document.querySelector('#playing-area');
 
 // Creating a function to create our board based on an input of rows and columns
-function createBoard(row, col) {
-  //create row first by ammending to playing area
-  for (let i = 1; i <= row; i++) {
-    let row = document.createElement('div');
-    playArea.appendChild(row);
-    row.className = 'row';
-    //ammend individual spots to each row, assign class of 'empty'
-    for (let j = 1; j <= columns; j++) {
-      let spot = document.createElement('div');
-      row.appendChild(spot);
-      spot.className = 'spot';
-      //Add class of empty to each spot
-      let spotClass = document.createElement('div');
-      spot.appendChild(spotClass);
-      spotClass.className = 'empty';
+function createBoard(col, row) {
+  //create column first by ammending to playing area
+  for (let i = 1; i <= col; i++) {
+    let column = document.createElement('div');
+    playArea.appendChild(column);
+    column.className = 'column';
+    //ammend individual spots to each column, assign class of 'empty'
+    for (let j = 0; j <= row; j++) {
+      if (j === 0) {
+        //The spot at the zero index will be different than the others. Not actually a part of the board, but where the player would go to drop the token
+        let dropSpot = document.createElement('div');
+        column.appendChild(dropSpot);
+        dropSpot.className = 'drop-area';
+      } else {
+        let spot = document.createElement('div');
+        column.appendChild(spot);
+        spot.className = 'spot';
+        //Add class of empty to each spot
+        let spotClass = document.createElement('div');
+        spot.appendChild(spotClass);
+        spotClass.className = 'empty';
+      }
     }
   }
 }
 
-createBoard(rows, columns);
+createBoard(columns, rows);
 
 playArea.addEventListener('click', (event) => {
   console.log(event.target);
